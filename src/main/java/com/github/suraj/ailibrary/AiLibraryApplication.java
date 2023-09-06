dcf473621548af169684498e2ff96ac7937f3575
Suraj Kopparam(laptop) <surajmsd1@gmail.com>
2023-09-06 14:04:05 -0400

    :rocket: first commit

---
+++ src/main/java/com/github/suraj/ailibrary/AiLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.github.suraj.ailibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/suraj/ailibrary/AiLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.suraj.ailibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
