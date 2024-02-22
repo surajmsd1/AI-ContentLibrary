e68104f06d379566aa475315a5496844b448d1f8
Suraj Kopparam <surajkopparam@Surajs-MBP.lan>
2024-02-22 13:34:24 -0500

    added origin to cors

--- backend/src/main/java/com/github/suraj/ailibrary/AiLibraryApplication.java
+++ backend/src/main/java/com/github/suraj/ailibrary/AiLibraryApplication.java
@@ -59,7 +59,8 @@ public class AiLibraryApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000","http://localhost:4200",
+				"https://www.neverforgetnotebook.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin","Content-Type",
 				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
